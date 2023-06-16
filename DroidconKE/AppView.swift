86748ca70e04bf2fc81ef24ffa570601dcb9f985
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-16 20:43:34 +0300

    Implemented splash logic

---
+++ DroidconKE/AppView.swift
@@ -0,0 +1,37 @@
+//
+//  AppView.swift
+//  DroidconKE
+//
+//  Created by Samuel Baraka on 16/06/2023.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    @State private var showSplash = true
+
+    var body: some View {
+        Group {
+            if showSplash {
+                SplashView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                showSplash = false
+                            }
+                        }
+                    }
+            } else {
+                // Add your main content view here
+                Text("Main Content")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }
+        }}
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
