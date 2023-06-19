5e284683b2593284c876245121b4e032e121c84f
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-19 13:08:07 +0300

    feat: Added home images (Banner and TextLogo)

---
+++ DroidconKE/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  DroidconKE
+//
+//  Created by Samuel Baraka on 19/06/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
