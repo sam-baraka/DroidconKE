339a7485fac5e06c348567689e12b0daa955e440
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-19 20:29:58 +0300

    modified the home view

--- DroidconKE/Home/HomeView.swift
+++ DroidconKE/Home/HomeView.swift
@@ -8,10 +8,49 @@
 import SwiftUI
 
 struct HomeView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+           NavigationView {
+               VStack (alignment: .leading){
+                   
+                   Text("Welcome to the largest Focussed Android Developer community in Africa")
+                       .multilineTextAlignment(.leading).padding(8)
+                   
+                   Image("HomeBanner")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .padding(8).frame(width: UIScreen.main.bounds.width)
+                   CallForSpeakersView().padding(8).frame(width: UIScreen.main.bounds.width)
+                   
+                   Spacer()
+               }
+               .toolbar {
+                   ToolbarItem(placement: .navigationBarLeading) {
+                       if colorScheme == .dark {
+                           Image("LogoTextDark").frame(width: 137,height: 25)
+                               .padding()
+                                  } else {
+                                      Image("LogoText").frame(width: 137,height: 25)
+                                          .padding()
+                                  }
+                      
+                   }
+                   
+                   ToolbarItem(placement: .navigationBarTrailing) {
+                       Button(action: {
+                           // Perform action for other button
+                       }) {
+                           Circle()
+                               .foregroundColor(Color("AccentColor"))
+                                           .frame(width: 30, height: 30)
+                                           .overlay(Image(systemName: "lock.fill").frame(width: 14,height: 14).foregroundColor(.white)).colorScheme(.light)
+                       }
+                   }
+               }
+           }
+       }
+   }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
