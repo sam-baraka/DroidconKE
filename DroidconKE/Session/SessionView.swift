871e06592310d9e6b86a173c182188b7455c1f0f
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-26 20:57:06 +0300

    added session

---
+++ DroidconKE/Session/SessionView.swift
@@ -0,0 +1,20 @@
+//
+//  SessionView.swift
+//  DroidconKE
+//
+//  Created by Samuel Baraka on 21/06/2023.
+//
+
+import SwiftUI
+
+struct SessionView: View {
+    var body: some View {
+        Text("Session View")
+    }
+}
+
+struct SessionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SessionView()
+    }
+}
