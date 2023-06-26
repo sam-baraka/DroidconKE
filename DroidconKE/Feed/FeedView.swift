aa988f040f29b2b58b24f3f1824c42d5edf0546c
Samuel Baraka <sababuvercetti@gmail.com>
2023-06-26 20:58:01 +0300

    Added feed view

---
+++ DroidconKE/Feed/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  DroidconKE
+//
+//  Created by Samuel Baraka on 21/06/2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text("Feed View")
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
