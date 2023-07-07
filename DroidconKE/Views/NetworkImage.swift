4c726d0cb01c6f5d3e2ae89f5585fd29b45afd20
Samuel Baraka <sababuvercetti@gmail.com>
2023-07-07 13:19:41 +0300

    created the sessions view

---
+++ DroidconKE/Views/NetworkImage.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkImage.swift
+//  DroidconKE
+//
+//  Created by Samuel Baraka on 07/07/2023.
+//
+
+import SwiftUI
+
+struct NetworkImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NetworkImage_Previews: PreviewProvider {
+    static var previews: some View {
+        NetworkImage()
+    }
+}
